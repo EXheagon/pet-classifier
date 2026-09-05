9bad13d2ad69caaf16348e9d33d9aed4c23ff2a8
EXheagon <1850261947@qq.com>
2026-09-05 16:10:27 +0800

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,1 +1,15 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
 
+
+def create_resnet18(num_classes=37):
+
+    model = models.resnet18(weights="DEFAULT")
+
+    model.fc = nn.Linear(
+        model.fc.in_features,
+        num_classes
+    )
+
+    return model
